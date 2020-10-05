bc80c1f47ed0543529f8586a932a9db49e5ebb79
markw13a <markw13a@gmail.com>
2020-10-05 11:31:42 +0100

    Removed Header as it was not implemented in the end

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Provider } from "react-redux";
 import { store } from "./state";
-import { Header } from "./components/Header";
 import { Categories } from "./components/Categories";
 import { ProductCards } from "./components/ProductCards";
 import { AddProductButton } from "./components/AddProductButton";
@@ -11,7 +10,6 @@ const App = () => {
   return (
     <Provider store={store}>
       <div className="app">
-        <Header />
         <div className="app__body">
           <Categories />
           <div className="app__hackyContainer">
